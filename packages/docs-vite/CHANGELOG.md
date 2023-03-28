a22e3d1d475ce9ce013edcbac96e9aac91cad626
lisen6 <1789502923@qq.com>
2023-03-28 09:13:58 +0800

    feat: add changset tool

---
+++ packages/docs-vite/CHANGELOG.md
@@ -0,0 +1,9 @@
+# docs-vite
+
+## 1.0.1
+
+### Patch Changes
+
+- Updated dependencies [afb34d5]
+- Updated dependencies
+  - @lisen6/smarty-ui-vite@0.2.3
--- packages/smarty-ui-vite/src/Button/Button.tsx
+++ packages/smarty-ui-vite/src/Button/Button.tsx
@@ -62,7 +62,7 @@ export default defineComponent({
       },
     };
 
-    console.log("我是button2");
+    console.log("我是button3");
     return () => (
       <button
         class={`
