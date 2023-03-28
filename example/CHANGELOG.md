c8d1d7d02d5f05305c804c1d1be1dcdb3847be30
lisen6 <1789502923@qq.com>
2023-03-28 22:43:36 +0800

    feat: button content to ci

--- example/CHANGELOG.md
+++ example/CHANGELOG.md
@@ -1,5 +1,12 @@
 # example
 
+## 0.0.4
+
+### Patch Changes
+
+- Updated dependencies
+  - @lisen6/smarty-ui-vite@0.1.6
+
 ## 0.0.3
 
 ### Patch Changes
--- packages/smarty-ui-vite/src/Button/Button.tsx
+++ packages/smarty-ui-vite/src/Button/Button.tsx
@@ -62,7 +62,7 @@ export default defineComponent({
       },
     };
 
-    console.log("new version");
+    console.log("test ci");
 
     return () => (
       <button
